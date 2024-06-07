0f6d5234a909c5ee1ec263f7820c6ceae441183d
Lucas Almeida <lucasalmeidagouvea123@gmail.com>
2024-06-07 18:43:58 -0300

    install dependencies

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+
+<template>
+     <div>
+        <Home />
+     </div>
+  <RouterView />
+</template>
+
+  <script>
+  import { RouterView } from 'vue-router'
+  import Home from './components/Home.vue'
+  </script>
